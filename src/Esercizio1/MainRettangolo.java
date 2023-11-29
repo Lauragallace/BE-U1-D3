5d5c7ab3a70c568fa011e6199313790c4532c287
Lauragallace <143189799+Lauragallace@users.noreply.github.com>
2023-11-29 16:29:40 +0100

    Esercizio1

---
+++ src/Esercizio1/MainRettangolo.java
@@ -0,0 +1,9 @@
+package Esercizio1;
+
+public class MainRettangolo {   public static void main(String[] arg){
+    Rettangolo ret1 = new Rettangolo(2,4);
+    Rettangolo ret2 = new Rettangolo(6,8);
+    Rettangolo.stampaRettangolo(ret1);
+    Rettangolo.stampaDueRettangoli(ret1,ret2);
+}
+}
